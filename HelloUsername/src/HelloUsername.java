881adc83287d8c15f856e4937ca52fbf2387af9f
jfc2705 <jfc2705@AN112494-CT103.nvstu.edu>
2015-09-15 15:41:59 -0400

    Hey

---
+++ HelloUsername/src/HelloUsername.java
@@ -0,0 +1,25 @@
+
+	import java.util.Scanner;
+	public class HelloUsername {
+
+		public static void main(String[] args) {
+			Scanner keyboard = new Scanner(System.in);
+			
+			String name;
+			System.out.println("Enter your name");
+			name = keyboard.next();
+			
+			String username;
+			System.out.println("Enter your username");
+			username = keyboard.next();
+			
+			String password;
+			System.out.println("Enter your password");
+			password = keyboard.next();
+			
+			System.out.println("Hello " + name + ",");
+			System.out.println("Your username is " + username);
+			System.out.print("Your password is " + password);
+			
+		}
+}

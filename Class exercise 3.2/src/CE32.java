fbbaeb298aad490b69b2ba57339472531ee3fbe7
CreEaToR <rockko10@gmail.com>
2015-09-23 14:18:35 -0400

    Initial

---
+++ Class exercise 3.2/src/CE32.java
@@ -0,0 +1,54 @@
+
+public class CE32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Jose Mendoza
+		 * This program will show case the use of print
+		 * 
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		char test = 'b';
+		
+		double pi = 3.14;
+		
+		double distance = 9.3E+10;
+		
+		double money = 5.30;
+		
+		//with strings
+	
+		System.out.printf("%s and %s","Hello","Welcome to java");
+		
+		System.out.println("");
+		
+		//with characters
+		
+		System.out.printf("The second letter in the alphabet is %c", test);
+
+		System.out.println("");
+		
+		//with decamals 
+		
+		System.out.printf("The value of pi is", pi);
+		
+		System.out.println("");
+		
+		//with exponential floating-point number
+
+		System.out.printf("The distance between the earth and the sun is %e", distance);
+		
+		System.out.println("");
+		
+		//floating number
+		
+		System.out.printf("The amount of money I have is $%8.2f", money);
+		
+	
+	}
+
+}
